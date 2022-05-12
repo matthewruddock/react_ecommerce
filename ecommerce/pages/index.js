fbbd833f4e3e43c5e2a11f1b4e0398a032252f51
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2022-05-12 16:44:52 -0500

    update

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -5,11 +5,11 @@ const Home = () => {
     <>
       HeroBanner
 
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className="products-container">
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
 
