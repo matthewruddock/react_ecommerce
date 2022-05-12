5827858d4e431e7c8716023151fc462679b1d3e1
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2022-05-12 17:07:56 -0500

    DONE

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -4,8 +4,8 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
-    <>
-      HeroBanner
+    <div>
+      <HeroBanner />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -15,9 +15,9 @@ const Home = () => {
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
 
-      Footer
-    </>
-  )
+      <FooterBanner/>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
