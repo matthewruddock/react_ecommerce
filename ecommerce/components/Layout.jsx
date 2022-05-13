c531c49a2f518bc46e28afde43df2449ba53a9b1
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2022-05-12 21:18:54 -0500

    updated

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layour = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layour</div>
+    <div className="layout">
+      <Head>
+        <title>Matthew Dev Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        { children }
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
-export default Layour
+export default Layout
