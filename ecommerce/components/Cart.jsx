34454ecf8d2e4730a44c9514efbec6f4d1a1cd02
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2022-05-13 11:10:31 -0500

    updated

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
-import { AiOutlineMinus, AiOutlineplus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
@@ -45,9 +45,42 @@ const Cart = () => {
             <div className="product" key={item._id}>
               <img src={urlFor(item?.image[0])}
               className="cart-product-image"/>
+              <div className="item-desc">
+                <h5>{item.name}</h5>
+                <h4>${item.price}</h4>
+              </div>
+              <div className="flex bottom">
+                <div>
+                  <p className="quantity-desc">
+                    <span className="minus" onClick=""><AiOutlineMinus /></span>
+                    <span className="num" onClick="">0</span>
+                    <span className="plus" onClick=""><AiOutlinePlus /></span>
+                  </p>
+                </div>
+                <button
+                type="button"
+                className="remove-item"
+                onClick=""
+                >
+                  <TiDeleteOutline />
+                </button>
+              </div>
             </div>
           ))}
         </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="">
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
 
       </div>
     </div>
