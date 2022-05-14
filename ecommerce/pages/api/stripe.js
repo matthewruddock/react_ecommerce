e1fbcb2fbbe9393be8bafe9eb2c93b927a9b23dd
matthewruddock <55115233+matthewruddock@users.noreply.github.com>
2022-05-13 20:02:27 -0500

    updated 3

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,46 +1,46 @@
 import Stripe from './stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    consol.log(req.body.cartItems)
     try {
-        const params = {
-            submit_type: 'pay',
-            mode: 'payment',
-            payment_method_types: ['card'],
-            billing_address_collection: 'auto',
-            shipping_options: [
-                { shipping_rate: 'shr_1Kz2tAFV9UFNUZyHtOyQbIpv'},
-                { shipping_rate: 'shr_1Kz2ttFV9UFNUZyHxUjGLjw3'},
-            ],
-            line_items: req.body.cartItems.map((item) => {
-              const img = item.image[0].asset._ref;
-              const newImage = img.replace('image-', 'https://cdn.sandity.io/images/r3k6skvi/production/').replace('-webp', '.webp');
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1Kz2tAFV9UFNUZyHtOyQbIpv'},
+          { shipping_rate: 'shr_1Kz2ttFV9UFNUZyHxUjGLjw3'},
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/r3k6skvi/production/').replace('-webp', '.webp');
 
-              return {
-                price_data: {
-                  currency: 'usd',
-                  product_data: {
-                    name: item.name,
-                    images: [newImage],
-                  },
-                  unit_amount: item.price * 100,
-                },
-                adjustable_quantity: {
-                  enabled: true,
-                  minimum: 1,
-                },
-                quantity: item.quantity
-              }
-              }),
-            success_url: `${req.headers.origin}/?success=true`,
-            cancel_url: `${req.headers.origin}/?canceled=true`,
+          return {
+            price_data: { 
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
           }
-        
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      }
+
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
+
       res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
